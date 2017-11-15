d624081493db47c21174e6ee9f64724d2d9e1c04
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-15 16:21:31 -0500

    Replace itemCounter with inline calculation

--- exercise.js
+++ exercise.js
@@ -1,9 +1,6 @@
 var items = ["purchase domain", "install SSL certificate", "deploy site", "celebrate success"];
 
-var itemCounter = 0;
-
 console.log("Todo:");
 for(var i = 0; i < items.length; i++){
-  itemCounter++;
-  console.log(itemCounter + ": " + items[i]);
+  console.log((i+1) + ": " + items[i]);
 }
